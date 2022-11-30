e0dff0a62a5fb6de189cf150fa119c222db9633b
Archerymidnight <kiernandougie@gmail.com>
2022-11-30 19:34:21 +0000

    Added fields to User class, mutators and accessors for each field

--- src/User.java
+++ src/User.java
@@ -1,2 +1,40 @@
 public class User {
+
+
+    private int userScore;
+    private String currentWord;
+    private int playerNumber;
+    private int turnPriority;
+
+    public void setUserScore(int newScore){
+        userScore = newScore;
+    }
+
+    public void setCurrentWord(String newWord){
+        currentWord = newWord;
+    }
+
+    public void setPlayerNumber(int newPlayerNumber){
+        playerNumber = newPlayerNumber;
+    }
+
+    public void setTurnPriority(int newPriority) {
+        turnPriority = newPriority;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public int getUserScore() {
+        return userScore;
+    }
+
+    public int getTurnPriority() {
+        return turnPriority;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
 }
